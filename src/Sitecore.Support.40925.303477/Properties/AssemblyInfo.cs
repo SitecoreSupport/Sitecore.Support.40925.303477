d17fd868a2c7910e0db7d7db4a582a5bc0f8af30
Andrey Filchenkov <22079019+AndreyFilchenkov@users.noreply.github.com>
2019-03-22 18:13:19 +0200

    Initial commit

---
+++ src/Sitecore.Support.40925.303477/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.40925.303477")]
+[assembly: AssemblyProduct("Sitecore.Support.40925.303477")]
+[assembly: ComVisible(false)]
